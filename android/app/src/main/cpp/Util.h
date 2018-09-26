855e308d8d0d88d005902900320b16603a42303a
XuJian1252878 <1252878xujian@tongji.edu.cn>
2018-09-26 09:43:57 +0800

    更新工具类Util.cpp

--- android/app/src/main/cpp/Util.h
+++ android/app/src/main/cpp/Util.h
@@ -8,7 +8,12 @@
 #endif //PHOTOR_UTIL_H
 
 #include <iostream>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <errno.h>
 #include <opencv/cv.hpp>
+#include <time.h>
 
 #include "opencv2/xfeatures2d.hpp"
 
@@ -16,7 +21,16 @@ using namespace cv;
 using namespace std;
 using namespace cv::xfeatures2d;
 
+Mat_<Vec3b> addMeanImgs(std::vector<Mat_<Vec3b>>& sourceImages);
 
 Mat_<Vec3b> superimposedImg(vector<Mat_<Vec3b>>& images, Mat_<Vec3b>& trainImg);
 
 Mat_<Vec3b> superimposedImg(Mat_<Vec3b>& queryImg, Mat_<Vec3b>& trainImg);
+
+/**
+ * 根据配准参数homo，获得queryImg根据配准参数 变换 后的图像
+ * @param queryImg
+ * @param homo
+ * @return
+*/
+Mat_<Vec3b> getTransformImgByHomo(Mat_<Vec3b>& queryImg, Mat homo);
