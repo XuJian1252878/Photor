93b7f24f0b3a1b623f3ac62381959b256f1e51db
XuJian1252878 <1252878xujian@tongji.edu.cn>
2018-09-27 15:58:42 +0800

    完成获取图片的exif信息

---
+++ android/app/src/main/java/com/photor/exposure/task/ExposureMergeThread.java
@@ -0,0 +1,16 @@
+package com.photor.exposure.task;
+
+import java.util.ArrayList;
+
+public class ExposureMergeThread extends Thread {
+
+
+//    private native int exposureMergePhotos(ArrayList<String> photos, ArrayList<Double> exposureTimes, long resImgAddr);
+
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+}
