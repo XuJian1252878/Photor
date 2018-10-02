f0e51e5dd05d6ee135b81038f3a6ab1fa544cffe
XuJian1252878 <1252878xujian@tongji.edu.cn>
2018-10-03 00:32:56 +0800

    整理FileUtils 代码

--- android/app/src/main/java/com/photor/imageedit/ImageEditActivity.java
+++ android/app/src/main/java/com/photor/imageedit/ImageEditActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.file.FileUtils;
 import com.photor.R;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.xinlan.imageeditlibrary.editimage.EditImageActivity;
